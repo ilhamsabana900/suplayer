538fbc5995c92e6c24bb8eff763395d18121ecbc
Ilhamsabana900 <ilhamsabana900>
2024-10-27 13:46:30 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+AOS.init();
+
+var typed = new Typed(".typing", {
+  strings: [
+    "Suplayer Peci Langsung Dari Tangan Pertama",
+    "Surga Peci Berkualitas dari Bangil Pasuruan",
+  ],
+  typeSpeed: 100,
+  delaySpeed: 300,
+  loop: true,
+  startDelay: 200,
+});
+
